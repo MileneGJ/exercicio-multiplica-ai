3786caf1516f92030c918d907b0b294689629599
milenegj <milene.jannetti@gmail.com>
2023-01-12 16:28:40 -0600

    Exercício resolvido

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int num = 7;
+
+        System.out.println("Tabela de multiplicação do " + num);
+
+        for(int i = 1; i<=10; i++) {
+            System.out.println(num + " x " + i + " = " + num*i);
+        }
+
+    }
+}
